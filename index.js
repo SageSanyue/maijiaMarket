f34283e6e8240cddfc3efb11e5cab21a100c9304
Sanyue2333 <562441461@qq.com>
2018-07-04 19:06:36 +0800

    在rap2引用的mock数据成功但img失败src有误

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-/*let url = 'http://http://rap2api.taobao.org/app/mock/data/36912'
+let url = 'http://rap2api.taobao.org/app/mock/7058/index/hotLists'
 
 //let axios = require('axios')
 
-axios.post(url,{
+/*axios.post(url,{
     pageNum: 1,
     pageSize: 10
 }).then(res=>{ 
     console.log(res.data.lists)
 })*/
 
-let Mock = require('mockjs')
+/*let Mock = require('mockjs')
 console.log(Mock.mock({
     "number|1-100.2":1
-}))
+}))*/
+
+new Vue({
+    el: '#app',
+    data: {
+        lists: null
+    },
+    created(){
+        axios.post(url,{
+            pageNum: 1,
+            pageSize: 10
+        }).then(res => {
+            console.log(res.data.lists)
+            this.lists = res.data.lists
+        })
+    }
+})
